2d0420d21455fdd663b5cbaa0cc513945c51c84c
Charlie <charlievanbeck@gmail.com>
2025-11-24 20:23:55 +0000

    Add home page

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const mysql = require("mysql2");
 const config = require("./utils/config")
 const ejs = require("ejs");
+const path = require("path")
+const mainRoutes = require("./routes/main")
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", mainRoutes)
+
+
+app.locals.surgery = { name: "Wynell Surgery" }
 
 module.exports = app;
