9cc421d9ed1a58c8fae407f257a93f7eda7b7df4
Charlie <charlievanbeck@gmail.com>
2025-11-24 20:42:28 +0000

    Add basic structure for routers

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,54 @@
 const router = require("express").Router();
 
+// All routers get passed through here
 router.get("/", (req, res) => {
     res.render("home.ejs");
 });
+
+router.get("/about", (req, res, next) => {
+    next();
+});
+
+router.get("/login", (req, res, next) => {
+    res.render("login.ejs");
+});
+
+router.post("/loginattempt", (req, res, next) => {
+    next()
+})
+
+router.get("/register", (req, res, next => {
+    res.render("register.ejs")
+}))
+
+router.post("/registered", (req, res, next) => {
+    next()
+})
+
+router.get("/dashboard", (req, res, next) => {
+    next()
+})
+
+router.get("/appointments", (req, res, next) => {
+    next()
+})
+
+router.get("/appointments/:id", (req, res, next) => {
+    next()
+})
+
+router.post("/addappointment", (req, res, next) => {
+    next()
+})
+
+router.post("/cancelappointment", (req, res, next) => {
+    next()
+})
+
+router.get("/search", (req, res, next) => {
+    next()
+})
+
+
 
 module.exports = router
