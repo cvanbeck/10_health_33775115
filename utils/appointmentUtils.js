12a846a01b332ffdb304e0b16d5f1c120c18beff
Charlie <charlievanbeck@gmail.com>
2025-11-26 17:32:51 +0000

    Fix typos

--- utils/appointmentUtils.js
+++ utils/appointmentUtils.js
@@ -13,19 +13,19 @@ function seperateUsersByRole(users) {
 
 function renderAppointments(res, next, id = -1) {
     let query = `SELECT
-                        appointments.id,
-                        appointments.time,
-                        appointments.date,
-                        p.first_name AS pfirst,
-                        p.last_name AS plast,
-                        d.first_name AS dfirst,
-                        d.last_name AS dlast,
-                        departments.name AS department
-                    FROM appointments
-                        INNER JOIN users as p ON appointments.patient_id = p.id
-                        INNER JOIN users as d ON appointments.doctor_id = d.id
-                        INNER JOIN doctors ON appointments.doctor_id = doctors.user_id
-                        INNER JOIN departments ON doctors.department_id = departments.id`;
+                    appointments.id,
+                    appointments.time,
+                    appointments.date,
+                    p.first_name AS pfirst,
+                    p.last_name AS plast,
+                    d.first_name AS dfirst,
+                    d.last_name AS dlast,
+                    departments.name AS department
+                FROM appointments
+                    INNER JOIN users as p ON appointments.patient_id = p.id
+                    INNER JOIN users as d ON appointments.doctor_id = d.id
+                    INNER JOIN doctors ON appointments.doctor_id = doctors.user_id
+                    INNER JOIN departments ON doctors.department_id = departments.id`;
 
     // Check if an id was specified, if so selects only that item 
     query += id === -1 ? "" : `WHERE appointments.id = ?`;
