c434898f3fe3b9e3a4eacb0168205ca564a73ff8
Charlie <charlievanbeck@gmail.com>
2025-11-26 15:18:25 +0000

    Add basic appointment booking system: Add dynamically generated list of doctors/patients that pulls from database. Add db query to create new appointment entries in database

--- routes/appointments.js
+++ routes/appointments.js
@@ -11,14 +11,47 @@ router.get("/:id", (req, res, next) => {
     next()
 })
 
+// Page to add new appointment
 router.get("/add", (req, res, next) => {
-    // Page to add new appointment
-    res.render("add.ejs")
+    let query = `
+        SELECT users.id, users.first_name, users.last_name, users.account_type, departments.name
+        FROM users 
+        LEFT JOIN doctors ON users.id = doctors.user_id
+        LEFT JOIN departments ON doctors.department_id = departments.id;
+    `
+    db.query(query, (err, result) => {
+        if (err) return next(err);
+
+        let info = {
+            patients: [],
+            doctors: []
+        }
+        result.forEach(user => {
+            user.account_type === "doctor" ? info.doctors.push(user) : info.patients.push(user)
+        })
+
+        res.render("add.ejs", { patients : info.patients, doctors : info.doctors })
+    })
 })
 
 router.post("/add", (req, res, next) => {
     // Submits to database
-    res.send("Appointment added succesful")
+
+    query = `
+        INSERT INTO appointments (patient_id, doctor_id, time, date)
+        VALUES(?, ?, CURTIME(), CURDATE())
+    `
+    let record = [
+        req.body.patient,
+        req.body.doctor
+    ]
+
+    db.query(query, record, (err, result) => {
+        console.log(result)
+        if (err) return next(err);
+        next()
+        res.send(`Appointment succesfully added`)
+    })
 })
 
 router.get("/cancel", (req, res, next) => {
