f7fd9b6c14b31fd607aa9b2960e36ad3cc647292
Charlie <charlievanbeck@gmail.com>
2025-11-26 19:23:54 +0000

    Update test data script to include multiple doctors and patients2

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -1,9 +1,11 @@
 USE appointment_booking;
 
-INSERT INTO departments (name)
-VALUES (
-    "Cardiology"
-);
+INSERT INTO departments
+    (name)
+VALUES 
+    ("Cardiology"),
+    ("Pediatrics"),
+    ("Radiology");
 
 INSERT INTO users (email, username, hashed_password, account_type, first_name, last_name)
 VALUES (
@@ -24,6 +26,40 @@ VALUES (
 
 INSERT INTO users (email, username, hashed_password, account_type, first_name, last_name)
 VALUES (
+    "secondDoc@ac.uk",
+    "Second",
+    "$2y$10$We22BggEZvgoGz/yoeVfWeL1IVmH3GJl8YfhuPZ/ccLbjmLME91BG",
+    "doctor",
+    "Dr",
+    "Acula"
+);
+
+INSERT INTO doctors (user_id, department_id, availability)
+VALUES (
+    LAST_INSERT_ID(),
+    2,
+    11
+);
+
+INSERT INTO users (email, username, hashed_password, account_type, first_name, last_name)
+VALUES (
+    "anotherDoc@ac.uk",
+    "Third",
+    "$2y$10$We22BggEZvgoGz/yoeVfWeL1IVmH3GJl8YfhuPZ/ccLbjmLME91BG",
+    "doctor",
+    "Emma",
+    "Royd"
+);
+
+INSERT INTO doctors (user_id, department_id, availability)
+VALUES (
+    LAST_INSERT_ID(),
+    3,
+    11
+);
+
+INSERT INTO users (email, username, hashed_password, account_type, first_name, last_name)
+VALUES (
     "patient@ac.uk",
     "illMan",
     "$2y$10$We22BggEZvgoGz/yoeVfWeL1IVmH3GJl8YfhuPZ/ccLbjmLME91BG",
@@ -38,15 +74,35 @@ VALUES (
     '1000-01-01'
 );
 
+INSERT INTO users (email, username, hashed_password, account_type, first_name, last_name)
+VALUES (
+    "anotherPatient@ac.uk",
+    "badHeadache",
+    "$2y$10$We22BggEZvgoGz/yoeVfWeL1IVmH3GJl8YfhuPZ/ccLbjmLME91BG",
+    "pat",
+    "Help",
+    "Me"
+);
 
-INSERT INTO appointments(
-    patient_id,
-    doctor_id,
-    time,
-    date
-) VALUES (
-    2,
-    1,
-    '11:11:11',
+INSERT INTO patients (user_id, date_of_birth)
+VALUES (
+    LAST_INSERT_ID(),
+    '1000-01-02'
+);
+
+INSERT INTO users (email, username, hashed_password, account_type, first_name, last_name)
+VALUES (
+    "oneMore@ac.uk",
+    "DyingMan",
+    "$2y$10$We22BggEZvgoGz/yoeVfWeL1IVmH3GJl8YfhuPZ/ccLbjmLME91BG",
+    "pat",
+    "Lotsof",
+    "Vomit"
+);
+
+INSERT INTO patients (user_id, date_of_birth)
+VALUES (
+    LAST_INSERT_ID(),
     '1000-01-01'
 );
+
